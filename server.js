132590bef225dff750c1508c4b34376712ae72c2
Anna Mendoza <annamendoza.curve@gmail.com>
2019-06-27 23:53:42 -0700

    fixing a prob the server

--- server.js
+++ server.js
@@ -13,16 +13,15 @@ app.use(cors());
 app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next){
- const authToken = req.get('Authorization') ? req.get('Authorization').split(' ')[1] : false;
+ const authToken = req.get('Authorization');
  const apiToken = process.env.API_TOKEN;
 
-	if( !authToken || authToken !== apiToken){
+	if( !authToken || authToken.split(' ')[1] !== apiToken){
 		return res.status(401).json({ error: 'Sorry Unauthorized request'})
 	}
 	// if all works then move on to the next middleware
 	next();
 });
-
 
 function handleGetMovies(req, res){
 	let response =  MOVIEDEX;
